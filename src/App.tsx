92c09a5d9f83718ae7f1c402ea39a43c2e5ffc23
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-28 21:10:45 +0530

    feat: integratoed react ace editor and react markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import Description from "./pages/Description/Description";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const markdownText = ` asasddddddddddddddddddddddddddddddddddddddddddddddddasssssssssssssssssssssss`;
+  return <Description descriptionText={markdownText} />;
 }
 
 export default App;
